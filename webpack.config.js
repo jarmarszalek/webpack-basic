f792c90b729669cd1a2b2707e713060a4dfede8f
jarmarszalek <jar.marszalek@gmail.com>
2020-03-03 23:45:21 +0100

    Init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
